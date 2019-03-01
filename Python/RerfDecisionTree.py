b99bdbee0723da04314265a462e80803698785e1
Ben Falk <falk.ben@gmail.com>
2019-03-01 17:33:07 -0500

    init sklearn class with needed methods and inputs

---
+++ Python/RerfDecisionTree.py
@@ -0,0 +1,16 @@
+import pyfp
+import numpy as np
+from sklearn import base
+
+
+class RerfDecisionTree(base.BaseEstimator):
+    """A randomer forest decision tree classifier."""
+
+    def __init__(self, max_depth=None, seed=None):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
