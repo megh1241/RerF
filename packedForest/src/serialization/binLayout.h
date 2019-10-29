eb4770df90476cadfb36f9b6a4dea6ad3419951f
EC2 Default User <ec2-user@ip-172-31-94-11.ec2.internal>
2019-10-29 22:51:16 +0000

    Add BINStatLayout

--- packedForest/src/serialization/binLayout.h
+++ packedForest/src/serialization/binLayout.h
@@ -132,6 +132,90 @@ template<typename T, typename Q>
 
             }
             
+            inline void BINStat(int depthIntertwined){
+                std::vector< fpBaseNode<T,Q> > bin = binstr.getBin();
+                std::map<int, int> nodeTreeMap = binstr.getNodeTreeMap();
+
+                int numNodesToProc = std::pow(2, depthIntertwined) - 2; 
+                int d;
+                auto numClasses = fpSingleton::getSingleton().returnNumClasses();
+                
+		if(depthIntertwined == 1){
+			for(auto i: bin)
+				finalbin.push_back(i);
+			return;
+		}
+		
+		
+		for(auto i = 0; i < numClasses; ++i){
+                    finalbin.push_back(bin[i]);
+                    nodeNewIdx.insert(std::pair<int, int>(bin[i].getID(), finalbin.size()-1));
+                }
+
+                for(auto i = 0; i < binstr.numOfTreesInBin; ++i)
+                    binQ.push_back(bin[i+numClasses]);
+
+                // Intertwined levels
+                int currLevel = 0; 
+                    //if(nodeTreeMap[binQ.front().getID()] != i)
+                      //  continue;
+                while(currLevel <= numNodesToProc*binstr.numOfTreesInBin) {
+                        currLevel += 2;
+                        auto ele = binQ.front();
+                        binQ.pop_front();
+                        finalbin.push_back(ele);
+                        nodeNewIdx.insert(std::pair<int, int>(ele.getID(), finalbin.size()-1));
+                        if((ele.returnLeftNodeID() < fpSingleton::getSingleton().returnNumClasses()) && (ele.returnRightNodeID() < fpSingleton::getSingleton().returnNumClasses()))
+                           continue;
+
+                        else if(ele.returnLeftNodeID() < fpSingleton::getSingleton().returnNumClasses())
+                            binQ.push_back(bin[ele.returnRightNodeID()]);
+
+                        else if(ele.returnRightNodeID() < fpSingleton::getSingleton().returnNumClasses())
+                            binQ.push_back(bin[ele.returnLeftNodeID()]); 
+
+                        else {
+                            binQ.push_back(bin[ele.returnLeftNodeID()]); 
+                            binQ.push_back(bin[ele.returnRightNodeID()]); 
+                        }
+                        
+                }
+
+                // STAT per (sub)tree layout 
+                
+                while(!binQ.empty()){
+                    std::deque<fpBaseNode<T, Q>> binST;
+                    auto ele = binQ.front();
+                    binQ.pop_front();
+                    binST.push_back(ele);
+                    while(!binST.empty()){
+                        auto ele = binST.front();
+                        binST.pop_front(); 
+                        finalbin.push_back(ele);
+                        nodeNewIdx.insert(std::pair<int, int>(ele.getID(), finalbin.size()-1));
+                        if((ele.returnLeftNodeID() < fpSingleton::getSingleton().returnNumClasses()) && (ele.returnRightNodeID() < fpSingleton::getSingleton().returnNumClasses()))
+                           continue;
+
+                        else if(ele.returnLeftNodeID() < fpSingleton::getSingleton().returnNumClasses())
+                            binST.push_back(bin[ele.returnRightNodeID()]);
+
+                        else if(ele.returnRightNodeID() < fpSingleton::getSingleton().returnNumClasses())
+                            binST.push_back(bin[ele.returnLeftNodeID()]); 
+
+                        else {
+                            binST.push_back(bin[ele.returnLeftNodeID()]); 
+                            binST.push_back(bin[ele.returnRightNodeID()]); 
+                        }
+                    }
+                }
+                auto siz = finalbin.size();
+                for (auto i=0; i<siz; i++){
+                    finalbin[i].setLeftValue(nodeNewIdx[bin[finalbin[i].returnLeftNodeID()].getID()]);
+                    finalbin[i].setRightValue(nodeNewIdx[bin[finalbin[i].returnRightNodeID()].getID()]);
+                }
+
+            }
+            
 
             inline void BFSLayout(){
                 std::vector< fpBaseNode<T,Q> > bin = binstr.getBin();
