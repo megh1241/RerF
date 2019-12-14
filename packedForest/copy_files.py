5611a5c056517a8a4aebfcb29f3f569eb4e9dee8
Ubuntu <ubuntu@ip-172-31-21-172.ec2.internal>
2019-12-14 17:35:54 +0000

    added script to copy files

---
+++ packedForest/copy_files.py
@@ -0,0 +1,9 @@
+import shutil 
+import os
+
+num_of_files = 900
+base_dir = '/data4'
+base_filename = 'bfsars0.bin'
+
+for i in range(num_of_files): 
+    shutil.copy2(os.path.join(base_dir, base_filename), '/data4/bfars{}.py'.format(i+1))
