44c8fd282da13834f52df5ead5f0a2d74a2aff05
Ubuntu <ubuntu@ip-172-31-23-63.ec2.internal>
2019-12-15 23:29:10 +0000

    fix bug

--- packedForest/copy_files.py
+++ packedForest/copy_files.py
@@ -6,4 +6,4 @@ base_dir = '/data4'
 base_filename = 'bfsars0.bin'
 
 for i in range(num_of_files): 
-    shutil.copy2(os.path.join(base_dir, base_filename), '/data4/bfars{}.bin'.format(i+1))
+    shutil.copy2(os.path.join(base_dir, base_filename), '/data4/bfsars{}.bin'.format(i+1))
