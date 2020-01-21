e565377297f288fae1d0d9e580ef9cf0231785ac
Ubuntu <ubuntu@ip-172-31-23-63.ec2.internal>
2020-01-21 15:18:17 +0000

    newfile

--- packedForest/experiment_bfs.sh
+++ packedForest/experiment_bfs.sh
@@ -1,39 +1,43 @@
 rm /data4/*
 make
-./bin/fp 7 8 1 bfs 1 train
+./bin/fp 7 7 1 binstat 3 train
 sudo echo 3 > sudo /proc/sys/vm/drop_caches
 make clean
 make
-./bin/fp 7 8 1 bfs 1 test
+./bin/fp 7 7 1 binstat 3 test
 
 rm /data4/*
 make
-./bin/fp 7 8 1 binstat 5 train
+./bin/fp 7 7 1 binstatclass 3 train
 sudo echo 3 > sudo /proc/sys/vm/drop_caches
 make clean
-make
-./bin/fp 7 8 1 binstat 5 test
+
 
 rm /data4/*
 make
-./bin/fp 7 8 1 binstatclass 5 train
+./bin/fp 7 7 1 binstatclass 3 test
+make
+./bin/fp 7 7 1 bfs 1 train
 sudo echo 3 > sudo /proc/sys/vm/drop_caches
 make clean
 make
-./bin/fp 7 8 1 binstatclass 5 test
+./bin/fp 7 7 1 bfs 1 test
+
 
 rm /data4/*
 make
-./bin/fp 7 8 1 stat 5 train
+./bin/fp 7 7 1 stat 3 train
 sudo echo 3 > sudo /proc/sys/vm/drop_caches
 make clean
 make
-./bin/fp 7 8 1 stat 5 test
+./bin/fp 7 7 1 stat 3 test
 
 rm /data4/*
+rm *.bin
+rm *.txt
 make
-./bin/fp 7 8 1 binbfs 5 train
+./bin/fp 7 7 1 binbfs 3 train
 sudo echo 3 > sudo /proc/sys/vm/drop_caches
 make clean
 make
-./bin/fp 7 8 1 binbfs 5 test
+./bin/fp 7 7 1 binbfs 3 test
